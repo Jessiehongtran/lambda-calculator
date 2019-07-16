310ab4033609bf7bbc0fb0b7fd5f0d94af9a79e1
Your Name <you@example.com>
2019-07-16 18:19:58 -0400

    imported and exported data, mapped over data for numbers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import Numbers from "./components/ButtonComponents/NumberButtons/Numbers"
+import NumberButton from "./components/ButtonComponents/NumberButtons/NumberButton"
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 
@@ -16,8 +18,10 @@ function App() {
   return (
     <div className="container">
       <Logo />
-      <div className="App">
+      <div className="App"> 
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        <Numbers/>
+        <NumberButton/>
       </div>
     </div>
   );
