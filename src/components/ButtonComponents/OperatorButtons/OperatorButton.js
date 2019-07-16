f0e4aaa687bf6672560c0e77fcc5dcdf6ef1977c
Your Name <you@example.com>
2019-07-16 18:53:21 -0400

    got Operator displayed on buttons

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-const OperatorButton = ({operator}) => {
+const OperatorButton = ({char,value}) => {
   return (
       // {/* Display a button element rendering the data being passed down from the parent container on props */}
     <button>
-      {operator}
+      {char}{value}
     </button>
   );
 };
