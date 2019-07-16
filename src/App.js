6b36a72ca85aa39e229dd694fa856c4612743bcd
Your Name <you@example.com>
2019-07-16 18:39:06 -0400

    mapped over data for Operators

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./App.css";
 import Numbers from "./components/ButtonComponents/NumberButtons/Numbers"
 import NumberButton from "./components/ButtonComponents/NumberButtons/NumberButton"
+import Operators from "./components/ButtonComponents/OperatorButtons/Operators"
+import OperatorButtons from "./components/ButtonComponents/OperatorButtons/OperatorButton"
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 
@@ -22,6 +24,8 @@ function App() {
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
         <Numbers/>
         <NumberButton/>
+        <Operators/>
+        <OperatorButtons/>
       </div>
     </div>
   );
